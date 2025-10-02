50e0149ba7f7c5779067a80bd69b7c8e47d454fe
siona <150366247+Siona3870@users.noreply.github.com>
2025-10-02 20:51:28 +0530

    Update Incident.sol

--- Incident.sol
+++ Incident.sol
@@ -3,23 +3,23 @@ pragma solidity ^0.8.19;
 
 contract IncidentReporting {
     uint256 public incidentCount = 0;
-
+    
     struct Incident {
         uint256 id;
         string lat;
         string lon;
         uint256 timestamp;
     }
-
+    
     mapping(uint256 => Incident) public incidents;
-
+    
     event IncidentReported(
         uint256 id,
         string lat,
         string lon,
         uint256 timestamp
     );
-
+    
     function reportIncident(string memory _lat, string memory _lon) public {
         incidentCount++;
         incidents[incidentCount] = Incident(
@@ -28,10 +28,10 @@ contract IncidentReporting {
             _lon,
             block.timestamp
         );
-
+        
         emit IncidentReported(incidentCount, _lat, _lon, block.timestamp);
     }
-
+    
     function getIncident(uint256 _id) public view returns (
         uint256,
         string memory,
