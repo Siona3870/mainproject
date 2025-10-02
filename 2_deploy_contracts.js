c4b6ffece06c5c5b0aa1b287cf3faaef4233349e
siona <150366247+Siona3870@users.noreply.github.com>
2025-10-02 20:51:52 +0530

    Update 2_deploy_contracts.js

--- 2_deploy_contracts.js
+++ 2_deploy_contracts.js
@@ -1,5 +1,13 @@
 const IncidentReporting = artifacts.require("IncidentReporting");
 
-module.exports = function (deployer) {
-  deployer.deploy(IncidentReporting);
+module.exports = async function (deployer) {
+  await deployer.deploy(IncidentReporting);
+  
+  const instance = await IncidentReporting.deployed();
+  console.log("\n===========================================");
+  console.log("Contract deployed successfully!");
+  console.log("Contract Address:", instance.address);
+  console.log("===========================================");
+  console.log("\nNow run: node update-contract-address.js");
+  console.log("===========================================\n");
 };
